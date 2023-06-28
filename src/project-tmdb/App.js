892178c861dc53397364ea50fa45e239b4159fc9
A-Kolesnikov <kolesnikov.spb@gmail.com>
2023-06-28 14:34:06 +0300

    added TV component

--- src/project-tmdb/App.js
+++ src/project-tmdb/App.js
@@ -1,10 +1,14 @@
 import '../App.css'
 import React from 'react'
+import TV from './TV'
 
 function App(){
     const fetch = require('node-fetch')
 
     const url = 'https://api.themoviedb.org/3/account/20072455'//'https://api.themoviedb.org/3/authentication'
+    const urlMov = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+    const urlTV = 'https://api.themoviedb.org/3/discover/tv?include_adult=false&include_null_first_air_dates=false&language=en-US&page=1&sort_by=popularity.desc';
+
     const options = {
         method: 'GET',
         headers:{
@@ -17,17 +21,17 @@ function App(){
     .then(res => res.json())
     .then(json => console.log(json))
     .catch(err => console.error('error' + err))*/
-
-    async function getInfo(){
+    async function getInfo(addr){
         try{
-            let response = await fetch(url, options)
+            let response = await fetch(addr, options)
             let result = await response.json()
-            console.log(result)
+            console.log (result)
         } catch(err){
             console.error('error' + err)
         }
     }
-    getInfo()
+
+    getInfo(urlMov)
 
     return(
         <div className='container'>
