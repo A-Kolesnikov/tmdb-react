86bc9c1cc86b45fc368c5a5c3efffa1ad028eb88
A-Kolesnikov <kolesnikov.spb@gmail.com>
2023-07-10 13:56:06 +0300

    upgraded styling

--- src/project-tmdb/Header.js
+++ src/project-tmdb/Header.js
@@ -1,16 +1,14 @@
 import React from "react";
 
-function Header(){
-    return(
+function Header() {
+    return (
         <div className="container-fluid">
             <div className="row">
                 <div className="d-flex
                 justify-content-center
-                allign-items-center w-100
-                p-3 header text-uppercase
-                ">
-
-                    Welcome to Tmdb
+                w-100
+                p-3 header text-uppercase">
+                    The Movies DataBase
                 </div>
             </div>
         </div>
